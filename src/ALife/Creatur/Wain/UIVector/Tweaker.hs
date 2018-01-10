1df1edd21139841ccb123c4f31400f3822bf1420
Amy de Buitléir <amy@nualeargais.ie>
2018-01-10 17:45:15 +0000

    added new statistics

--- src/ALife/Creatur/Wain/UIVector/Tweaker.hs
+++ src/ALife/Creatur/Wain/UIVector/Tweaker.hs
@@ -42,4 +42,4 @@ instance W8.Genetic PatternTweaker
 instance Diploid PatternTweaker
 
 instance Statistical PatternTweaker where
-  stats (PatternTweaker ws) = map (prefix "tweaker weights") . stats $ ws
+  stats (PatternTweaker ws) = map (prefix "tweaker weight") . stats $ ws
